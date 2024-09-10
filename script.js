b0d2e977524282b769ee08bc72ee5820a2a06180
farhan <ff1917914@gmail.com>
2024-09-10 12:25:19 -0700

    Added more htmls

---
+++ script.js
@@ -0,0 +1,117 @@
+// chapter no 1 
+
+
+// Q1
+
+// document.write("sum of 3 and 5 is 8")
+
+
+// Q2
+
+// smjh nh aya 
+
+// Q3
+
+// document.write("value after variable declaration is undefind <br> intial value: 5<br> value after incremet is: 6 <br> value after addition is: 13<br> value after decrement is: 12 <br> the reminder is: 0")
+
+// Q4
+
+// document.write("total cost to buy 5 tickets to a movie is 3000PKR")
+
+// Q5
+
+
+// document.write("table of 4 <br> 4x1=4 <br>  4x2=8 <br>  4x3=12 <br>  4x4=16 <br>  4x5=20 <br>  4x6=24 <br>   4x7=28 <br>  4x8=32 <br>  4x9=36 <br>  4x10=40 <br>")
+
+
+// Q6
+
+// document.write("°C = (°f - 32) x 5/9   <br>  °f = (°c x 9/5) + 32 ")
+
+// Q7
+
+// document.write("25°C is 77 °f \n 70°f is 21.11111111111111°C")
+
+// Q8
+
+// document.write("price of item  1 is 650 <br>  Qauntity of item 1 is 3 <br> price of item 2 is 100 <br>  quantity of  item 2 is 7 <br> shipping charges 100 <br> <br> <br> <br> total cost of your order is 2750")
+
+// Q9
+
+
+// document.write("total marks: 980 <br> marks obtained: 804 <br> percentage: 84.0408163265306% ")
+
+// Q10
+
+// document.write("total cuurency in PKR: 1748")
+
+// Q11
+
+// document.write("current year: 2016<br> birth year: 1992<br> your age is: 24")
+
+// Q12
+//  document.write("radius of a circle: 20 <br> the circumference is: 125.6799999999999")
+
+// Q13
+
+// document.write("favourite snake: chocolate chip <br>  current age: 15 <br> estimated maximum age: 65 <br> amount of snake per day: 3 <br> you will need 150 chocolate chip to last you until the ride old age of: 65")
+
+
+
+// chapter no 5 complete ////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chapter no 6 and 9 
+
+
+// Q1
+
+// document.write("result: <br> the value of a is: 10 <br> .............................. <br> the value of ++a  is: 11 <br> now the value of a is: 11 <br> the value of a++ is: 11 <br> now the value of a is: 12 <br>  the value of --a is: 11 <br>  now the value of a is: 11 <br> the value of a-- is: 11 <br> now the value of a is: 10 ")
+
+
+// Q2
+
+
+// var a = 2
+// var b = 1
+
+// --a = 1
+// --a - --b = 1
+// --a - --b + ++b = 1
+// --a - --b + ++b + b-- = 1
+
+// document.write("a = 2 <br> b = 1 <br> result: 1")
+
+
+// Q3
+
+
+// var input = prompt("enter your name")
+
+
